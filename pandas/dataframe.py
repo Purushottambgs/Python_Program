a340b63ac2cd75e7c5c3118e9338298c8b983ffa
Purushottam <bgspurushottamjha29@gmail.com>
2026-04-18 23:24:58 +0530

    ignore dataset folder

---
+++ pandas/dataframe.py
@@ -0,0 +1,5 @@
+import numpy as np
+import pandas as pd
+
+batsman=pd.read_csv('Dataset/batsman_runs_ipl.csv')
+batsman.head()
---
+++ positive_negative_zero.py
@@ -0,0 +1,7 @@
+num= int(input("enter a number:- "))
+if num>0:
+    print("it is possitive number")
+elif num==0:
+    print("it is a zero number")
+else:
+    print("it is a negative number")
