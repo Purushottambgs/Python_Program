a977f6f78299249b166fff4419dc954988249f91
Purushottam <bgspurushottamjha29@gmail.com>
2026-04-27 00:26:11 +0530

    add

---
+++ count.py
@@ -0,0 +1,11 @@
+s=input("Enter the text:- ")
+term=input("what would like to search:- ")
+
+coun= s.split().count(term)
+
+
+# for i in s:
+#     if i==term:
+#         count+=1
+
+print("Frequency:-", coun)
--- palindrome.py
+++ palindrome.py
@@ -1,13 +1,34 @@
 
-
 s= input("Enter the string:- ")
-flag= True
 
-for i in range(0,len(s)//2):
-    if s[i] != s[len(s)- i-1]:
-        flag=False
-        print("not prime")
-        break
+# if s==s[::-1]:
+#     print('it number is palindrome')
+# else:
+#     print('it is not palindrome number')
+
+
+
+flag=True
+
+for i in range (0, len(s)//2):
+    s[i]!= s[len(s)-i-1]
+    flag = False
+    print("not palindrome")
+    break
 
 if flag:
-    print("Prime Number")
+    print("number is palindrome")
+
+
+
+
+# flag= True
+
+# for i in range(0,len(s)//2):
+#     if s[i] != s[len(s)- i-1]:
+#         flag=False
+#         print("not palindrome")
+#         break
+
+# if flag:
+#     print("Palindrome number")
